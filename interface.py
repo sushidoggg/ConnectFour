0d9f9eb2a1110cf1fdbb550f14dcae5b0d68bbb1
OliviaY1 <yanqianyanxi@outlook.com>
2023-03-29 20:13:13 -0400

    Create draw_window() function
    Unfinished function

--- interface.py
+++ interface.py
@@ -1,12 +1,44 @@
-import pygame
 from __future__ import annotations
+from connect_four import ConnectFour
+import pygame
 
-SQUARESIZE = ...
-BOARD_WIDTH, BOARD_HEIGHT = SQUARESIZE * 7, SQUARESIZE * (6 + 1)
-BUTTOM_COLUMN_WIDTH =
-SCREEN_WIDTH, SCREEN_HEIGHT = BOARD_WIDTH + ..., BOARD_HEIGHT
+
+SQUARESIZE= 75
+RADIUS = int(SQUARESIZE / 3)
+WINDOW_WIDTH, WINDOW_HEIGHT = SQUARESIZE * 11, SQUARESIZE * 11
+BUTTOM_COLUMN_WIDTH = ...
 FONT = ...
 COLOR_PLAYER_ONE, COLOR_PLAYER_TWO = (0, 255, 255), (255, 0, 0)
+BLUE, WHITE, BLACK = (0, 0, 255), (255, 255, 255), (0,0,0)
+ROW_COUNT, COLUMN_COUNT = 6, 7
+
+def draw_board(window: pygame.Surface) -> None:
+    '''Based on the given sqaure size, draw an interface that has a 6*7 board on the given window'''
+    window.fill(WHITE)
+    pygame.display.flip()
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(window, BLUE, ((c + 1)* SQUARESIZE, (r + 2) * SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(window, WHITE, (
+                int((c + 1) * SQUARESIZE + SQUARESIZE / 2), int((r + 2) * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+    pygame.display.update()
+
+# 模拟main里面的window
+pygame.display.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Connect Four")
+# main while loop
+def draw_window(window: pygame.Surface, game: ConnectFour):
+    draw_board(window)
+    # draw a disc based on who plays and the positon
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+
+
+
 
 class Disc():
     '''A class that represents the disc that players played
@@ -15,18 +47,15 @@ class Disc():
         - color: list[tuple]
     Representation Invariants:
         - color in {COLOR_PLAYER_TWO, COLOR_PLAYER_ONE}'''
-
+    ...
 
 class Player(pygame.sprite.Sprite):
     '''A class that represents Player.
     # todo：return the column int player choose'''
+    ...
 
 class AI(pygame.sprite.Sprite):
     ...
-def draw_window(sqau re_size: int) -> pygame.Surface:
-    '''Based on the given sqaure size, draw an interface that has a 6*7 board, buttons on the right'''
-    ...
-
 def events_simulator(game: ConnectFour):
     '''Run events while not game_over. Use event.handle_events(ConnectFour)
     Preconditions:
@@ -35,3 +64,4 @@ def events_simulator(game: ConnectFour):
 
 def is_valid_position() -> bool:
 # return the user's move as an integer to connect_four.py.
+    ...
