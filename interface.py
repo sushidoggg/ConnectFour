33718a869647a6f9fe5eeba8853774e5d2b31711
OliviaY1 <yanqianyanxi@outlook.com>
2023-03-29 22:46:26 -0400

    Update interface.py

--- interface.py
+++ interface.py
@@ -4,6 +4,20 @@ from connect_four import ConnectFour
 from main import SQAURESIZE, RADIUS, WINDOW_WIDTH, WINDOW_HEIGHT, COLOR_PLAYER_ONE, COLOR_PLAYER_TWO, BLUE, WHITE, BLACK, ROW_COUNT, COLUMN_COUNT
 import pygame
 
+# todo: click the two buttons and see if it is user
+class Button():
+    """A class represents a circle buttons."""
+
+    def __init__(self, x: int, y: int, image: str) -> None:
+        """Create a button of given image at (x, y)"""
+        self.image = image
+        self.center = (x, y)
+        self.clicked = False
+    def draw(self, window: pygame.Surface) -> None:
+
+
+
+
 def draw_one_disc(window: pygame.Surface, color: tuple[int], center: tuple[int]) -> None:
     """Draw a beautiful disc on window at the given window with given color"""
     ...
@@ -21,6 +35,7 @@ def draw_window(window: pygame.Surface, game: ConnectFour) -> None:
                 int((c + 1) * SQUARESIZE + SQUARESIZE / 2), int((r + 2) * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
+            # TODO: convert grid-index to pygame's index
             if grid[r][c] == 1:  # Player Two's disc
                 pygame.draw.circle(window, COLOR_PLAYER_TWO, (
                     int((c + 1) * SQUARESIZE + SQUARESIZE / 2), int((r + 2) * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
@@ -30,8 +45,12 @@ def draw_window(window: pygame.Surface, game: ConnectFour) -> None:
     pygame.display.update()
 
 
+
+
+
 if __name__ == '__main__':
     # 模拟main里面的window
+
     pygame.display.init()
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("Connect Four")
@@ -40,27 +59,3 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-
-
-
-class Disc():
-    '''A class that represents the disc that players played
-    Instance Attributes:
-        - position: list[tuple]
-        - color: list[tuple]
-    Representation Invariants:
-        - color in {COLOR_PLAYER_TWO, COLOR_PLAYER_ONE}'''
-    ...
-
-class Player(pygame.sprite.Sprite):
-    '''A class that represents Player.
-    # todo：return the column int player choose'''
-    ...
-
-class AI(pygame.sprite.Sprite):
-    ...
-
-
-def is_valid_position() -> bool:
-# return the user's move as an integer to connect_four.py.
-    ...
