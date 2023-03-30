d51711fd35510efcbe4bd8405d55654a0724c997
OliviaY1 <yanqianyanxi@outlook.com>
2023-03-29 21:36:57 -0400

    Update interface.py
    draw_window function is finished

--- interface.py
+++ interface.py
@@ -1,42 +1,58 @@
 from __future__ import annotations
 from connect_four import ConnectFour
+from main import SQAURESIZE, RADIUS, WINDOW_WIDTH, WINDOW_HEIGHT, COLOR_PLAYER_ONE, COLOR_PLAYER_TWO, BLUE, WHITE, BLACK, ROW_COUNT, COLUMN_COUNT
 import pygame
 
+<<<<<<< Updated upstream
 
 SQUARESIZE= 75
+=======
+'''SQUARESIZE= 75
+>>>>>>> Stashed changes
 RADIUS = int(SQUARESIZE / 3)
 WINDOW_WIDTH, WINDOW_HEIGHT = SQUARESIZE * 11, SQUARESIZE * 11
 BUTTOM_COLUMN_WIDTH = ...
 FONT = ...
 COLOR_PLAYER_ONE, COLOR_PLAYER_TWO = (0, 255, 255), (255, 0, 0)
 BLUE, WHITE, BLACK = (0, 0, 255), (255, 255, 255), (0,0,0)
-ROW_COUNT, COLUMN_COUNT = 6, 7
+ROW_COUNT, COLUMN_COUNT = 6, 7'''
 
-def draw_board(window: pygame.Surface) -> None:
-    '''Based on the given sqaure size, draw an interface that has a 6*7 board on the given window'''
+def draw_one_disc(window: pygame.Surface, color: tuple[int], center: tuple[int]) -> None:
+    '''Draw a beautiful disc on window at the given window with given color'''
+    ...
+
+def draw_window(window: pygame.Surface, game: ConnectFour) -> None:
+    '''Based on the given sqaure size, draw the whole interface on the given window at the current status of game
+        If game.grid are all unoccupied, then just draw the window'''
     window.fill(WHITE)
     pygame.display.flip()
+    grid = game.grid
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
             pygame.draw.rect(window, BLUE, ((c + 1)* SQUARESIZE, (r + 2) * SQUARESIZE, SQUARESIZE, SQUARESIZE))
             pygame.draw.circle(window, WHITE, (
                 int((c + 1) * SQUARESIZE + SQUARESIZE / 2), int((r + 2) * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            if grid[r][c] == 1:  # Player Two's disc
+                pygame.draw.circle(window, COLOR_PLAYER_TWO, (
+                    int((c + 1) * SQUARESIZE + SQUARESIZE / 2), int((r + 2) * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+            elif grid[r][c] == 0:  # Player One's disc
+                pygame.draw.circle(window, COLOR_PLAYER_ONE, (
+                    int((c + 1) * SQUARESIZE + SQUARESIZE / 2), int((r + 2) * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
     pygame.display.update()
 
-# 模拟main里面的window
-pygame.display.init()
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Connect Four")
-# main while loop
-def draw_window(window: pygame.Surface, game: ConnectFour):
-    draw_board(window)
-    # draw a disc based on who plays and the positon
+
+if __name__ == '__main__':
+    # 模拟main里面的window
+    pygame.display.init()
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("Connect Four")
+    # main while loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-
-
 
 
 
@@ -56,10 +72,6 @@ class Player(pygame.sprite.Sprite):
 
 class AI(pygame.sprite.Sprite):
     ...
-def events_simulator(game: ConnectFour):
-    '''Run events while not game_over. Use event.handle_events(ConnectFour)
-    Preconditions:
-        - ConnectFour.first_player_moves is empty'''
 
 
 def is_valid_position() -> bool:
