d1f42398920405871032944835d126dd392c696e
AmandaWu226 <amandawu109@gmail.com>
2023-03-29 20:19:44 -0400

    Update connect_four.py

--- connect_four.py
+++ connect_four.py
@@ -1,12 +1,25 @@
-"""
+"""CSC111 Winter 2023 Project: Connect 4
+
 Module Description
 ==================
-# TODO: Write a Docstring
+
+This module contains a collection of Python classes and functions that represent the game of Connect 4.
+This file contains classes including ConnectFour, Player, RandomPlayer and AIPlayer, with associated functions
+defined under each class. By reading the *docstring* of this file, you can gain insights into the
+role and functionality of these classes and functions
+as well as how they contribute to this project as a whole.
 
 Copyright and Usage Information
 ===============================
-# TODO
 
+This file is provided solely for the personal and private use of the
+Teaching Stream of CSC111 at the University of Toronto St. George campus.
+All forms of distribution of this code, whether as given or with any changes, are
+expressly prohibited.
+
+This file is Copyright (c) 2023 Yige (Amanda) Wu, Sunyi (Alysa) Liu, Lecheng (Joyce) Qu, and Xi (Olivia) Yan.
+Additionally, this file references a2_adversarial_wordle.py from CSC111 Assignment 2,
+which is also Copyright (c) 2023 Mario Badr, David Liu, and Angela Zavaleta Bernuy.
 """
 
 from __future__ import annotations
