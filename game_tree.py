44fa6d5dad2f5fc8360f1f22af6851d831c5751f
JoyceQu <qulecheng2004@163.com>
2023-04-02 23:41:02 -0400

    Updated docstrings

--- game_tree.py
+++ game_tree.py
@@ -33,19 +33,21 @@ class GameTree:
     Each node in the tree stores a possible ConnectFour column.
 
     Instance Attributes:
-        - column: An int representing the current move (of either player_one or player_two),
-        or '*' if this tree represents the start of a game.
-        - player: Either PLAYER_ONE or PLAYER_TWO indicating which player is doing this move.
-        - score: A float between 0.0 to 1.0 (inclusive), representing how this move is favorable to self.player.
+        - move_column: An int representing the current move (of either PLAYER_ONE or PLAYER_TWO),
+        or a str '*' if this node represents the start of a game.
+        - initial_player: The player number of the AI player.
+        - current_player: The player that will choose a move in GameTree's subtree. In other words, current_player
+        is the opposite of the player who make the move that is stored in move_column.
+        - score: An integer representing how this move is favorable to the initial_player.
 
+    Private Instance Attributes:
+        - _subtrees: A mapping of a move columnt to its corresponding GameTree objects representing the
+        subtrees of this game tree.
 
     Representation Invariants:
-        - column == GAME_START_MOVE or 0 <= self.column < 7
-        - self.column == GAME_START_MOVE or self.player in {PLAYER_ONE, PLAYER_TWO}
-        - 0.0 <= self.score <= 1.0
-    # TODO: Introduce minimum score & average score
-    # TODO: Player
-    Current player will choose one of the possible moves in its subtress.
+        - self.move_column == GAME_START_MOVE or 0 <= self.column < 7
+        - self.move_column == GAME_START_MOVE or self.current_player in {PLAYER_ONE, PLAYER_TWO}
+        - self.initial_player in {PLAYER_ONE, PLAYER_TWO}
     """
     move_column: int | str
     initial_player: int
@@ -58,28 +60,16 @@ class GameTree:
         """ Initialize a new game tree.
 
         Precondition:
-            - column == GAME_START_MOVE or 0 <= column < 7
-            - player in {PLAYER_ONE, PLAYER_TWO}
+            - move_column == GAME_START_MOVE or 0 <= move_column < 7
+            - move_column == GAME_START_MOVE or current_player in {PLAYER_ONE, PLAYER_TWO}
+            - initial_player in {PLAYER_ONE, PLAYER_TWO}
+            - current_player is None or current_player in {PLAYER_ONE, PLAYER_TWO}
         """
         self.move_column = move_column
         self.initial_player = initial_player
         self.current_player = current_player
         self.score = score
         self._subtrees = {}
-
-    def get_subtrees(self) -> list[GameTree]:
-        """Return the subtrees of this game tree."""
-        return list(self._subtrees.values())
-
-    def get_subtree_by_column(self, column: int) -> Optional[GameTree]:
-        """Return the subtree corresponding to the given column.
-
-        Return None if no subtree corresponds to that column.
-        """
-        if column in self._subtrees:
-            return self._subtrees[column]
-        else:
-            return None
 
     def __str__(self) -> str:
         """Return a string representation of this tree.
@@ -107,16 +97,33 @@ class GameTree:
             str_so_far += subtree._str_indented(depth + 1)
         return str_so_far
 
+    def __len__(self) -> int:
+        """Return the number of items in this tree."""
+        return 1 + sum(subtree.__len__() for subtree in self._subtrees.values())
+
+    def get_subtrees(self) -> list[GameTree]:
+        """Return the subtrees of this game tree."""
+        return list(self._subtrees.values())
+
+    def get_subtree_by_column(self, column: int) -> Optional[GameTree]:
+        """Return the subtree corresponding to the given column.
+
+        Return None if no subtree corresponds to that column.
+
+        Preconditions:
+            - 0 <= move_column < 7
+        """
+        if column in self._subtrees:
+            return self._subtrees[column]
+        else:
+            return None
+
     def get_next_player(self) -> int:
         """Return the player who should move next."""
         if self.move_column == GAME_START_MOVE:
             return PLAYER_ONE
         else:
             return get_opposite_player(self.current_player)
-
-    def __len__(self) -> int:
-        """Return the number of items in this tree."""
-        return 1 + sum(subtree.__len__() for subtree in self._subtrees.values())
 
     def add_subtree(self, subtree: GameTree) -> None:
         """Add a subtree to this game tree."""
@@ -125,13 +132,20 @@ class GameTree:
 
     def update_score(self) -> None:
         """ Update the score for each new move.
+
+        Do nothing if self is a leaf node.
+
+        If current player is initial player, then initial player is going to choose between moves in subtrees.
+        We want to assume that the opponent player will make the best choice (by minimizing initial player's
+        score), so self's score is the minimum of its subtree's score.
+
+        Otherwise, the opponent is going to choose the next move. We know that the initial player (controlled
+        by us) will make the best move by maximizing intial player's score, so self's score is the maximum of
+        its subtree's score.
         """
         if len(self._subtrees) == 0:
-            # Do nothing when self is a leaf node
             return
 
-        # Choose the maximum score among all subtrees and reverse it to be self's score.
-        # TODO: Write a docstring and explain why
         if self.initial_player != self.current_player:
             max_subtree_score = max(subtree.score for subtree in self.get_subtrees())
             self.score = max_subtree_score
